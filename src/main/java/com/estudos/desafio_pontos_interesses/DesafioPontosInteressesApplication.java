8a695af82018569a63816f4ae740a7d680fc9fce
Guilherme Felix <f3lixguilherme@gmail.com>
2025-10-09 17:08:33 -0300

    first commit

---
+++ src/main/java/com/estudos/desafio_pontos_interesses/DesafioPontosInteressesApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.desafio_pontos_interesses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPontosInteressesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPontosInteressesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/desafio_pontos_interesses/DesafioPontosInteressesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.desafio_pontos_interesses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPontosInteressesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
